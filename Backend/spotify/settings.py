4724b89fe1938d15325692935cd1a80e6ba42b68
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-11 19:09:27 +0330

    Configure PostgreSQL database

--- Backend/spotify/settings.py
+++ Backend/spotify/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "spotify.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spotify_db',
+        'USER': 'amirreza',
+        'PASSWORD': 'seyyedamirreza1381',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
