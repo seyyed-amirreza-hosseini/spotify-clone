929edd407233b71a41e230f81808748716c36b66
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-12 11:27:57 +0330

    Implement signup functionality

--- Backend/music/views.py
+++ Backend/music/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -9,7 +13,33 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('password2')
+
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email is already taken")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "Username is already taken")
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_login = authenticate(request, username=username, password=password)
+            
+                auth_login(request, user_login)
+                return redirect('/')
+        else:
+            messages.info(request, "Passwords do not match")
+            return redirect('signup')
+        
+    else:
+        return render(request, 'signup.html')
 
 def logout(request):
     return render(request, 'logout.html')
