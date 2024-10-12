17f667b7baad2760e7090a5be036b56399e107fd
seyyed_amirreza_hosseini <hosseiniseyyedamirreza2003@gmail.com>
2024-10-12 11:36:19 +0330

    Add login required decorator to home page

--- Backend/music/views.py
+++ Backend/music/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
 def index(request):
     return render(request, 'index.html')
 
